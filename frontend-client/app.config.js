36976435b43e18651e4271107263bce1c8d68ca1
PrasenShinde <prasenshinde8@gmail.com>
2026-07-25 13:42:17 +0530

    refactor : app.config.js changes 2.0

--- frontend-client/app.config.js
+++ frontend-client/app.config.js
@@ -29,7 +29,7 @@ const iosClientIdPrefix = iosClientId.split('.').shift() || '';
 module.exports = {
   expo: {
     name: "AlphaMinds",
-    slug: "ott_7k",
+    slug: "7k",
     scheme: [
       "7k",
       androidClientIdPrefix ? `com.googleusercontent.apps.${androidClientIdPrefix}` : null,
